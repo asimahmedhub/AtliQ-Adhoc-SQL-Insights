d1737fce4ede04be9bfdfecf348499a3aa5d736a
asimahmedhub <asim.atia@gmail.com>
2025-10-02 21:47:41 -0500

    Create Request 1.sql

---
+++ Request 1.sql
@@ -0,0 +1,5 @@
+ # Provide the list of markets in which customer  "Atliq  Exclusive"  operates its business in the  APAC  region. 
+
+SELECT distinct market FROM gdb023.dim_customer
+	WHERE customer = "Atliq Exclusive"
+    AND region = "APAC";
